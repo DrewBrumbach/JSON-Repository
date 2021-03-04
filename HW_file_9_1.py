e3bd5d875895b036d542359c6b13027db038d864
Drew Brumbach <Drew_Brumbach1@baylor.edu>
2021-03-03 22:26:52 -0600

    9_1_fires

--- HW_file_9_1.py
+++ HW_file_9_1.py
@@ -8,4 +8,44 @@ json.dump(fi_data, outfile, indent=4)
 
 list_of_fis = fi_data
 
-print(list_of_fis)
+lats = []
+lons = []
+brights = []
+
+for fi in list_of_fis:
+    lat = fi["latitude"]
+    lon = fi["longitude"]
+    bright = fi["bright_t31"]
+
+    lats.append(lat)
+    lons.append(lon)
+    brights.append(bright)
+
+print(lats[:10])
+print(lons[:10])
+print(brights[:10])
+
+# graph
+from plotly.graph_objs import Scattergeo, Layout  # Remeber case sensitive
+from plotly import offline
+
+data = [
+    {
+        "type": "scattergeo",
+        "lat": lats,
+        "lon": lons,
+        "marker": {
+            "size": [5 * bright for bright in brights],
+            "color": brights,
+            "colorscale": "Viridis",
+            "reversescale": True,
+            "colorbar": {"title": "Brightness"},
+        },
+    }
+]
+
+my_layout = Layout(title="West Coast Fires")
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="west_coast_fires.html")
